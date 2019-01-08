d22cea514ecea9a0fdf2c579304ac485541b83da
moldingtofu <jeremykchang@gmail.com>
2019-01-08 17:36:52 -0500

    basic functionality tip percent change does not change total

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Tip Calculator
 //
-//  Created by jeremy on 1/8/19.
+//  Created by Jeremy on 1/8/19.
 //  Copyright © 2019 Jeremy Chang. All rights reserved.
 //
 
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func onChange(_ sender: Any) {
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double(billField.text!) ?? 0
+        let tips = [0.15, 0.18, 0.2]
+        let tip = bill * tips[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
